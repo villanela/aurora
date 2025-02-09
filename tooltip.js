d35e8ea07c3035fa4a86172705bd8c7c09adaf73
villanela <souththth@usa.com>
2025-02-09 11:47:52 -0300

    Create tooltip.js

---
+++ tooltip.js
@@ -0,0 +1,10 @@
+(function($){
+         $(document).ready(function(){
+             $("[title]").style_my_tooltips({ 
+    tip_follows_cursor:true,
+    tip_delay_time:300,
+    tip_fade_speed:300,
+    attribute:"title"
+});
+         });
+     })(jQuery);
